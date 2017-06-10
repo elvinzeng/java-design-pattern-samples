4958c28d0f3d4294769e585e8ddfeba93a9536c8
Elvin Zeng <zenghui@188.com>
2017-06-11 02:01:52 +0800

    修改构建脚本以及主类名称

--- simple-factory/src/main/java/com/gelvt/gofdp/simplefactory/App.java
+++ simple-factory/src/main/java/com/gelvt/gofdp/simplefactory/App.java
@@ -3,7 +3,7 @@ package com.gelvt.gofdp.simplefactory;
 /**
  * Created by Elvin Zeng on 17-6-11.
  */
-public class Main {
+public class App {
     public static void main(String[] args){
         System.out.println("hello simple factory!");
     }
