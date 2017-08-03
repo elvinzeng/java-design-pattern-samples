aeb874468895822337c26e5fabdac87a8138fcc3
Elvin Zeng <zenghui@188.com>
2017-08-03 22:19:35 +0800

    更新备忘录模式代码

--- memento/src/main/java/com/gelvt/gofdp/App.java
+++ memento/src/main/java/com/gelvt/gofdp/App.java
@@ -11,15 +11,15 @@ import com.gelvt.gofdp.memento.VersionManager;
 public class App {
     public static void main(String[] args) {
         Editor editor = new Editor();
-        VersionManager history = new VersionManager();
+        VersionManager versionManager = new VersionManager();
 
         editor.setAuthor("Elvin Zeng");
         editor.setContent("第一个版本");
-        history.add(editor.snapshoot());
+        versionManager.add(editor.snapshoot());
 
         editor.setAuthor("Elvin Zeng");
         editor.setContent("第二个版本");
-        history.add(editor.snapshoot());
+        versionManager.add(editor.snapshoot());
 
         editor.setAuthor("zenghui");
         editor.setContent("第三个版本");
@@ -27,11 +27,11 @@ public class App {
         System.out.println("编辑器当前状态：" + editor);
 
         System.out.println("撤回上一个版本");
-        editor.revertSnapshot(history.pop());  //  可以通过Ctrl+Z热键触发这段代码
+        editor.revertSnapshot(versionManager.pop());  //  可以通过Ctrl+Z热键触发这段代码
         System.out.println("编辑器当前状态：" + editor);
 
         System.out.println("撤回上一个版本");
-        editor.revertSnapshot(history.pop());
+        editor.revertSnapshot(versionManager.pop());
         System.out.println("编辑器当前状态：" + editor);
     }
 
