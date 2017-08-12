3a3952cde325dd5b4646db6a2914717f1b028d61
Elvin Zeng <zenghui@188.com>
2017-08-12 23:02:13 +0800

    fix build error

--- interpreter/src/main/java/com/gelvt/gofdp/App.java
+++ interpreter/src/main/java/com/gelvt/gofdp/App.java
@@ -1,31 +1,13 @@
 package com.gelvt.gofdp;
 
-import com.gelvt.gofdp.iterator.Iterator;
-import com.gelvt.gofdp.iterator.LinkedList;
-import com.gelvt.gofdp.iterator.List;
-
 /**
  * Client
  * @author: Elvin Zeng
- * @date: 17-8-5.
+ * @date: 17-8-12.
  */
 public class App {
     public static void main(String[] args) {
-        //  注意这里的List是本程序中自己实现的，不是Java库自带的那个。
-        List<String> list = new LinkedList<String>();
 
-        list.add("first");
-        list.add("second");
-        list.add("third");
-        list.add("fourth");
-
-        System.out.println("列表是否为空：" + list.isEmpty());
-        System.out.println("列表当前元素总数：" + list.size());
-        System.out.println("以下是列表中的所有元素");
-        Iterator<String> iterator = list.iterator();
-        while(iterator.hasNext()){
-            System.out.println(iterator.next());
-        }
     }
 
 }
--- interpreter/src/main/java/com/gelvt/gofdp/interpreter/Haha.java
+++ interpreter/src/main/java/com/gelvt/gofdp/interpreter/Haha.java
@@ -1,18 +1,9 @@
 package com.gelvt.gofdp.iterator;
 
 /**
- * 迭代器
+ *
  * @author: Elvin Zeng
- * @date: 17-8-5.
+ * @date: 17-8-12.
  */
-public interface Iterator<E> {
-    /**
-     * @return 是否还有下一个元素(概念与数学中集合元素一样)
-     */
-    boolean hasNext();
-
-    /**
-     * @return 下一个元素(概念与数学中集合元素一样)
-     */
-    E next();
+public interface Haha {
 }
